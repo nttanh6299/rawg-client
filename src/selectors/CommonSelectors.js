7c1cdd7b8d3258750dfe6472a371ca38add52e90
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-05-12 21:47:31 +0700

    change default genre

--- src/selectors/CommonSelectors.js
+++ src/selectors/CommonSelectors.js
@@ -1,3 +1,8 @@
 export const getGames = state => state.games;
 
-export const getGenre = state => state.router.route.options.genre || '';
+export const getGenre = state =>
+  state.router.route.options.query
+    ? ''
+    : state.router.route.options.genre || 'action';
+
+export const getSearch = state => state.router.route.options.query;
