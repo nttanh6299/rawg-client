d38a82c17352bf453479616e40983ab6e61325a3
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-05-29 14:11:15 +0700

    not found page styles

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { GiGamepadCross } from 'react-icons/gi';
 
 const NotFound = () => {
-  return <div className="not-found">Page not found</div>;
+  return (
+    <div className="not-found">
+      <div className="not-found__404">
+        <span>4</span>
+        <GiGamepadCross />
+        <span>4</span>
+      </div>
+      <span className="not-found__title">Page not found</span>
+    </div>
+  );
 };
 
 export default NotFound;
