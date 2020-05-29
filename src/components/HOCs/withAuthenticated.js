a3218a9f85fbeae9b6fc936c2dfb908f754d8761
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-05-29 19:52:26 +0700

    fix authen

--- src/components/HOCs/withAuthenticated.js
+++ src/components/HOCs/withAuthenticated.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import Loading from '../Loading';
 
-const withAuthenticated = (InnerComponent, redirectTo = '/') => {
+const withAuthenticated = (InnerComponent, redirectTo = '/login') => {
   const Authenticated = ({ currentUser, isAuthenticated, ...props }) => {
     if (!isAuthenticated) {
       return <Loading loading={true} className="u-text-center" />;
-    } else if (isAuthenticated && currentUser) {
+    } else if (isAuthenticated && !currentUser) {
       return <Redirect to={redirectTo} />;
     }
     return (
